00c1473bc5730d3c8e99a3f946aa1208b350cd25
Mugisha Kevine <Kevinemug>
2023-10-11 14:58:24 +0200

    main branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,6 @@
-import React from "react"
-import Navbar from "./components/Navbar"
-import Main from "./components/Main"
 
 export default function App() {
-    const [darkMode, setDarkMode] = React.useState(true)
-    
-    function toggleDarkMode() {
-        setDarkMode(prevMode => !prevMode)
-    }
-    
     return (
-        <div className="container">
-            <Navbar 
-                darkMode={darkMode} 
-                toggleDarkMode={toggleDarkMode}
-            />
-            <Main darkMode={darkMode} />
-        </div>
+        <h1>main branch</h1>
     )
 }
