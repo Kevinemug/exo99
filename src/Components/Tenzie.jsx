a32370fcf6a717847c298b05e0d948550ebef152
Mugisha Kevine <Kevinemug>
2023-10-12 10:38:24 +0200

    new changes

--- src/Components/Tenzie.jsx
+++ src/Components/Tenzie.jsx
@@ -69,7 +69,7 @@ export default function App() {
     return (
         <main>
  {tenzies && <Modal isActive={tenzies} onClose={closeModal} />}   
-          <h1 className="title">Tenzies</h1>
+          <h1 className="title">Tenzies game</h1>
             <p className="instructions">Roll until all dice are the same. 
             Click each die to freeze it at its current value between rolls.</p>
             <div className="dice-container">
